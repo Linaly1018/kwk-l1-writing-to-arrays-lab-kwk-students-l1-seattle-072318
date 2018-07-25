7bbc72d725ccb68bc425b28c4323bf26a51a0cc7
Linaly1018 <miyamoto.linaly@gmail.com>
2018-07-25 19:21:16 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -13,12 +13,12 @@ def change_rainbow_colors(rainbow_colors)
   puts rainbow_colors[2]
 end
 
-def add_colors(rainbow_colors)
-  rainbow_colors[3]="green"
-  puts rainbow_colors[3]
-  rainbow_colors[4]="blue"
-  puts rainbow_colors[4]
+def add_colors(change_rainbow_colorsrainbow_colors)
+  change_rainbow_colors[3]="green"
+  puts change_rainbow_colors[3]
+  change_rainbow_colors[4]="blue"
+  puts change_rainbow_colors[4]
 end
 
 print change_rainbow_colors(rainbow_colors)
-print add_colors(rainbow_colors)
+print add_colors(change_rainbow_colors)
